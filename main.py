c1333bcf06ba67d93b955089daac5468bcce326f
Muhammad -Awais-Tabassum <matabassum7277@gmail.com>
2025-06-21 14:14:39 +0500

    Test with manual pump.fun token

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from scanner.monitor import monitor
+from scanner.monitor import analyze
 
-if __name__ == "__main__":
-    monitor()
+# Manual test token
+test_token = {
+    'name': 'ManualTest',
+    'address': 'kWBjUhuK1cH495LmNdsFEdgmduWPsC6DK7eEEX8pump'
+    'deployer': '2VPh8q'
+}
+
+analyze(test_token)
