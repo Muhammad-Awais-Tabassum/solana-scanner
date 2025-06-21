7cceab23b9e415165c9524fb7bc1be25a6dbf9d4
Muhammad -Awais-Tabassum <matabassum7277@gmail.com>
2025-06-21 14:28:01 +0500

    Clean fixed main.py for test

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from scanner.monitor import analyze
 
-# Manual test token
 test_token = {
-    'name': 'ManualTest',
-    'address': '9FYxGMwfnQKdLcuW29t6Fd7ZwhQQVD3K12MdZUp34hps',
-    'deployer': '2VPh8q'
+    "name": "ManualTest",
+    "address": "9FYxGMwfnQKdLcuW29t6Fd7ZwhQQVD3K12MdZUp34hps",
+    "deployer": "2VPh8q"
 }
 
 analyze(test_token)
